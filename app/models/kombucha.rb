127a45467f189c64a8db901fd4018563ff257265
no0ro <nrolwes@gmail.com>
2020-08-11 20:13:07 -0400

    added associations

--- app/models/kombucha.rb
+++ app/models/kombucha.rb
@@ -1,4 +1,9 @@
 class Kombucha < ApplicationRecord
   belongs_to :brand
-  belongs_to :user
+  belongs_to :user #the creator of it #could rename belongs_to creator
+  # ^ gives us all the singular instances 
+  has_many :reviews
+  has_many :users, through: :reviews #ppl who have reviewed it
+  # ^ gives us all the plural instances
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :reveiws #must come before "through" below, bc we need to tell it that reviews exists first
+    has_many :kombuchas, through: :reviews 
+
+    has_many :kombuchas #that they created
 end
+ 
