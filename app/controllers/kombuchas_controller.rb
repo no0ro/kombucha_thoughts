b1a535cc206afa2bd596b3eba2dd7049f49ac48a
no0ro <nrolwes@gmail.com>
2020-09-18 20:22:11 -0400

    created kombucha vview files

--- app/controllers/kombuchas_controller.rb
+++ app/controllers/kombuchas_controller.rb
@@ -1,2 +1,26 @@
 class KombuchasController < ApplicationController
+
+    def index
+        @kombucha = Kombucha.all
+    end 
+
+    def show 
+    end 
+
+    def new 
+        @kombucha = Kombucha.new
+    end 
+
+    def create 
+        @kombucha = Kombucha.create(kombucha_params)
+    end 
+
+    def edit 
+    end 
+
+    def update 
+    end 
+
+    def destroy 
+    end 
 end
