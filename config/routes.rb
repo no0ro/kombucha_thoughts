0bb8404285a01eb7b08242433be294fe92340724
no0ro <nrolwes@gmail.com>
2020-09-25 21:34:20 -0400

    added /login /signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  get '/' => "session#welcome"
-  
+  get '/' => "sessions#welcome"
+
+  get '/login' => "sessions#new"
+  post '/login' => "sessions#create"
+
+  get '/signup' => "users#new"
+  post '/signup' => "users#create" 
+
+  delete '/logout' => "sessions#destroy"
+
   
   resources :reviews
   resources :kombuchas
+
   resources :brands
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
