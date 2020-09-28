401949b851df8666d2950b9e8cb7e6cf8fd6c20a
no0ro <nrolwes@gmail.com>
2020-09-28 11:25:52 -0400

    added link to create a new kombucha

--- app/controllers/kombuchas_controller.rb
+++ app/controllers/kombuchas_controller.rb
@@ -4,31 +4,31 @@ class KombuchasController < ApplicationController
     # these are the only route you should be able to go to use without having to authenticate (aka public could see)  
     
     # so you cant create a new kombucha, or delete, without logging in 
-    def index
-        @kombucha = Kombucha.all
-    end 
+    # def index
+    #     @kombucha = Kombucha.all
+    # end 
 
-    def show 
-        @kombucha = Kombucha.find(params[:id])
-    end 
+    # def show 
+    #     @kombucha = Kombucha.find(params[:id])
+    # end 
 
     def new 
         @kombucha = Kombucha.new
     end 
 
-    def create 
-        @kombucha = Kombucha.create(kombucha_params) 
-        # @kombucha = Kombucha.new(params[:kombucha]) -> doesnt work! 
-            # bc need to make private below, and then pass that into .new()
+    # def create 
+    #     @kombucha = Kombucha.create(kombucha_params) 
+    #     # @kombucha = Kombucha.new(params[:kombucha]) -> doesnt work! 
+    #         # bc need to make private below, and then pass that into .new()
 
-        if(@kombucha.save)
-            #&& @kombucha.is_valid?
-            redirect_to @kombucha
-        else 
-            puts "---> didnt save, rendered new again"
-            render 'new'
-        end 
-    end 
+    #     if(@kombucha.save)
+    #         #&& @kombucha.is_valid?
+    #         redirect_to @kombucha
+    #     else 
+    #         puts "---> didnt save, rendered new again"
+    #         render 'new'
+    #     end 
+    # end 
 
     def edit 
     end 
@@ -39,7 +39,7 @@ class KombuchasController < ApplicationController
     def destroy 
     end 
 
-    private def kombucha_params 
-        params.require(:kombucha).permit(:flavor, :description, :buy_again) 
-    end 
+    # private def kombucha_params 
+    #     params.require(:kombucha).permit(:flavor, :description, :buy_again) 
+    # end 
 end
