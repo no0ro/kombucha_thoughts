d10766bbe9eb8fab586e03d40921011f575ff08d
no0ro <nrolwes@gmail.com>
2020-09-28 01:58:22 -0400

    built out login create  method, with error message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,22 @@ class SessionsController < ApplicationController
     def new
     end
     
+    def create 
+        # does the user exist in our system? 
+        @user = User.find_by(username: params[:user][:username]) # find the user in our system via (key: value)
+        
+        
 
+        # once we find the user, ask: 
+        # if user exists in system AND the password input matches
+        if @user.try(:authenticate, params[:user][:password])
+            # if @user && @user.authenticate(password: params[:user][:password]) <- this did not work!
+            session[:user_id] = @user.id # set the user id (2) to be saved as the session. store them in our session. this is officially how we say theyre logged in 
+            redirect_to user_path(@user)
+        else
+            flash[:error] = "Please try again. Login information is incorrect."
+            redirect_to login_path # impt to redirect here! so username isnt persisted
+        end 
 
-end 
+    end 
+end  
