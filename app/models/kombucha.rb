6506b4ab90e30c34e837d27095cec7830a576547
no0ro <nrolwes@gmail.com>
2020-10-15 23:23:57 -0400

    scope method to alphabetize flavor- in drop down menu

--- app/models/kombucha.rb
+++ app/models/kombucha.rb
@@ -1,6 +1,6 @@
 class Kombucha < ApplicationRecord
   belongs_to :brand
-  belongs_to :user #the creator of it #could rename belongs_to creator
+  belongs_to :user # the creator of it #could rename belongs_to creator
   # ^ gives us all the singular instances 
   has_many :reviews
   has_many :users, through: :reviews #ppl who have reviewed it
@@ -9,6 +9,21 @@ class Kombucha < ApplicationRecord
   # validations
   validates :flavor, presence: :true
   validate :not_a_duplicate
+
+
+
+  # Instance Methods 
+
+  # written as class method - order alphabetically 
+  def self.alpha 
+    #order('flavor: :asc') 
+
+    order(Arel.sql("#{:flavor} ASC"))
+
+  end 
+
+  # written as scope method 
+  
 
 
   # brand_attributes (allows 2 versions of Brand to be properly saved to db)
@@ -21,20 +36,21 @@ class Kombucha < ApplicationRecord
     # this allows the kombucha model to change the Brand by passing a hash key brand_attributes
     # this still be there ???
 
+  
+  # same -- validates :kombucha, uniqueness: { scope: :user, message: "has already been reviewed by you"}
   def not_a_duplicate
     # if there is already a kombucha with that flavor && brand, throw an error
     if Kombucha.find_by(flavor: flavor, brand_id: brand_id)
       errors.add(:flavor, "has already been added to that brand")
     end 
   end 
-  # same -- validates :kombucha, uniqueness: { scope: :user, message: "has already been reviewed by you"}
 
   # for collection_select display in form
   def flavor_and_brand 
     "#{flavor} - #{brand.name}"
+     # "#{flavor} - #{brand.try(:name)}"
   end 
-  # "#{flavor} - #{brand.try(:name)}"
-
+ 
 
    
  
