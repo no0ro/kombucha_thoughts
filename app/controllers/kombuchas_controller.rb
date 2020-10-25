592fe32141a7285075f9248a60ffb67c60e132d4
no0ro <nrolwes@gmail.com>
2020-10-25 01:58:27 -0400

    edit link in show view and update mthod in controller

--- app/controllers/kombuchas_controller.rb
+++ app/controllers/kombuchas_controller.rb
@@ -1,8 +1,22 @@
 class KombuchasController < ApplicationController
-    before_action :set_kombucha
+    before_action :set_kombucha, only:[:show, :edit, :update, :destroy]
     before_action :redirect_if_not_logged_in 
     # so you cant create a new kombucha, or delete, without logging in 
- 
+
+
+    # GET /kombuchas
+    def index
+        # @kombuchas = Kombucha.all
+        @kombuchas = Kombucha.order_by_rating.includes(:brand)
+    end 
+
+    # GET /kombuchas/1
+    def show 
+        # vs .find
+        @kombucha = Kombucha.find_by_id(params[:id])
+    end 
+
+    # GET /kombuchas/new
     def new 
         @kombucha = Kombucha.new # 1st, create a new instance of Kombucha
         @kombucha.build_brand  # 2nd, bc we have a nested form, we also need to create a new instance of an Brand, and associate that Brand with the Kombucha. this is accomplished with the build method. 
@@ -14,7 +28,7 @@ class KombuchasController < ApplicationController
         # the build syntax used here: this is a belongs_to relationship syntax for .build - kombucha belongs to a brand 
     end 
 
-    
+    # POST /kombuchas
     def create 
         @kombucha = Kombucha.new(kombucha_params)
         @kombucha.user_id = session[:user_id] # pulling from the session id,(aka user.id) to then assign that to the creater of this new kombucha
@@ -28,21 +42,19 @@ class KombuchasController < ApplicationController
         end 
     end 
 
-    def index
-        # @kombuchas = Kombucha.all
-        @kombuchas = Kombucha.order_by_rating.includes(:brand)
-    end 
-
-
-    def show 
-        # vs .find
-        @kombucha = Kombucha.find_by_id(params[:id])
-    end 
-
+ 
+    # GET  /kombuchas/1/edit
     def edit 
     end 
 
+    # PATCH/PUT /kombuchas/1
     def update 
+        if @kombucha.update(kombucha_params)
+            redirect_to kombucha_path(@kombucha)
+            #notice: "Kombucha successfully updated"
+        else 
+            render :edit
+        end 
     end 
 
     # def destroy 
@@ -59,7 +71,7 @@ class KombuchasController < ApplicationController
     end 
 
     def set_kombucha
-        @kombucha= Kombucha.find_by(params[:id])
+        @kombucha = Kombucha.find_by(id: params[:id])
         redirect_to kombuchas_path if !@kombucha
      end
 end
