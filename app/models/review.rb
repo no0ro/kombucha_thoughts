98c1b2b40f99c7c8aa3ff76a8ea6f794ce2e9212
no0ro <nrolwes@gmail.com>
2020-10-28 15:41:56 -0400

    added helper date_formatted to Reviews Model and used in Views

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,13 +7,17 @@ class Review < ApplicationRecord
   validates :title, presence: true 
   validates :content, presence: true 
   validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 6}
- 
-  # validatios: use titlecase 
 
   # validate kombucha and our uniquness scope determins if user has already reviewed
   validates :kombucha, uniqueness: { scope: :user, message: "has already been reviewed by you"}
 
   # validates: , uniqueness: {scope :name, -> { where(:attibute => value)}
   # Ex:- scope :active, -> {where(:active => true)}}
+
+ def date_formatted
+  created_at.strftime("%m-%d-%Y")
+  # use on - a REVIEW instance - ie: review.date_formatted
+  # found in -  Views: users/show, reviews/index, reviews/show
+ end 
 
 end
