e8ae71e9fc64e25b4fb895ceadc01b78f922711c
no0ro <nrolwes@gmail.com>
2020-11-03 21:32:22 -0500

    added custom font to stylesheet and updated in views

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -2,9 +2,9 @@ module ReviewsHelper
     def display_header(review)
         # in views/reviews/edit.html.erb
         if params[:kombucha_id] 
-            content_tag(:h1, "Write a Review for:  #{@review.kombucha.flavor} -  #{@review.kombucha.brand.name}", {class: "lead text-center"}) 
+            content_tag(:h1, "Write a Review for:  #{@review.kombucha.flavor} -  #{@review.kombucha.brand.name}", {class: "shiftH3  text-center"}) 
         else 
-            content_tag(:h1, "Write a Review", {class: "lead text-center"})
+            content_tag(:h1, "Write A Review", {class: "shiftH1 text-center"})
         end 
     end 
 end
