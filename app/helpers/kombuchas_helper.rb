3276d26012d6837e0207d54e65fd5623557d9174
no0ro <nrolwes@gmail.com>
2020-11-06 19:19:27 -0500

    moved views logic for 'editt_kombucha_if_creator' into reviews_helper

--- app/helpers/kombuchas_helper.rb
+++ app/helpers/kombuchas_helper.rb
@@ -1,2 +1,8 @@
 module KombuchasHelper
+
+    def edit_kombucha_if_creator
+        if logged_in? && @kombucha.user_id == current_user.id
+            content_tag(:h6, (link_to 'Edit Kombucha', edit_kombucha_path(@kombucha)), {class: "card-subtitle mb-2 shiftp text-muted small"} )
+        end
+    end
 end
