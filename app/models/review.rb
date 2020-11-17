eb11573f1fc6c895cb4f589d8e778984fb684da3
no0ro <nrolwes@gmail.com>
2020-11-17 18:37:24 -0500

    added most_reviews scope method

--- app/models/review.rb
+++ app/models/review.rb
@@ -12,8 +12,13 @@ class Review < ApplicationRecord
   validates :kombucha, uniqueness: { scope: :user, message: "has already been reviewed by you"}
 
  # Methods
-
  scope :newest_reviewed, -> { order('created_at DESC').limit(3) }
+
+ scope :most_reviews, -> {joins(:kombucha).group('reviews.kombucha_id').order("count(reviews.kombucha_id) desc").first}
+ # RETURNS => #<Review id: 10, content: "Yummy I love this!", title: "Love", rating: 5, user_id: 4, kombucha_id: 2, created_at: "2020-10-21 18:37:33", updated_at: "2020-11-10 19:19:53">
+
+ scope :order_by_rating, -> {left_joins(:reviews).group(:id).order('avg(rating) desc')}
+
 
  def date_formatted
   created_at.strftime("%m-%d-%Y")
