3c85696453b9d23314f442300ba72518abbc4f18
no0ro <nrolwes@gmail.com>
2020-11-17 19:03:38 -0500

    edited comments

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -78,7 +78,7 @@ class ReviewsController < ApplicationController
     def most_reviews
         @review = Review.most_reviews 
         @kombucha = Kombucha.find_by_id(@review.kombucha_id)
-            # added @kombucha to easically get the total number of reviews it has
+            # added @kombucha to easily get the total number of reviews it has
     end 
 
     # DELETE /reviews/1 
